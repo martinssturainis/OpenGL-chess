b6ea5df256e37357bb2b8d66462f821b34b39990
martinssturainis <71926334+martinssturainis@users.noreply.github.com>
2021-08-13 12:39:20 +0300

    Update SpriteRenderer.cpp

--- engine/SpriteRenderer.cpp
+++ engine/SpriteRenderer.cpp
@@ -10,12 +10,15 @@ void SpriteRenderer::drawSprite(Textured2D texture, glm::vec2 pos, glm::vec2 siz
 {
 	this->shader.use();
 
-	// convert uni data
-	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(pos, 0.0f));
-	model = glm::scale(model, glm::vec3(size.x, size.y, 1.0f)); // Last scale
+	// init transformation matrix for postition and scale
+	glm::mat4 model = glm::mat4(1.0f); 
+	// init position. Tagad, lai pavrvietotu pietiks ar vec2 pos(x,y). z vienmer bus 0, jo stradajam ar 2d
+	model = glm::translate(model, glm::vec3(pos, 0.0f)); 
+	// init scale. Tagad, lai mainitu izmerus, pietiks ar vec2 size(x,y)
+	model = glm::scale(model, glm::vec3(size.x, size.y, 1.0f)); 
+	
 	// send uni data to shader
-	shader.sendMatrix4fv("model", model);
+	shader.sendMatrix4fv("model", model); 
 	shader.sendFloat4fv("newColor", color);
 
 	glActiveTexture(GL_TEXTURE0);
@@ -27,15 +30,17 @@ void SpriteRenderer::drawSprite(Textured2D texture, glm::vec2 pos, glm::vec2 siz
 
 void SpriteRenderer::initialize()
 {
+	// 2 trissturi, lai izveidotu 1 cetrsturi. Pietiktu ar 4 punktiem, bet es biju parak slinks, lai rakstītu indices
 	GLfloat vertices[] =
 	{
-		0.0f, 1.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 0.0f,
+		// pos      // uv texture corrd
+		0.0f, 1.0f,  0.0f, 1.0f,
+		1.0f, 0.0f,  1.0f, 0.0f,
+		0.0f, 0.0f,  0.0f, 0.0f,
 
-		0.0f, 1.0f, 0.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 0.0f, 1.0f, 0.0f
+		0.0f, 1.0f,  0.0f, 1.0f,
+		1.0f, 1.0f,  1.0f, 1.0f,
+		1.0f, 0.0f,  1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
